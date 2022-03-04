1a3f1aeacacbd4a199b0a34332cf2118ee0fcd67
Abdulaziz930 <abdulazizak@code.edu.az>
2022-03-04 17:17:52 +0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            while (true)
+            {
+                Console.WriteLine("bir eded daxil edin: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number < 0)
+                    continue;
+
+                while (number > 0)
+                {
+                    number /= 10;
+                    count++;
+                }
+
+                Console.WriteLine(count);
+
+                break;
+            }
+        }
+    }
+}
